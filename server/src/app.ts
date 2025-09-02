1f47b63c887c0f1526a96d991b4e3192652eba2f
Julie Luciani <julie.lcny@gmail.com>
2025-09-02 09:48:09 +0200

    add swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 import temperatureRoutes from "./routes/temperatureRoutes";
 import humidityRoutes from "./routes/humidityRoutes";
 import movementRoutes from "./routes/movementRoutes";
+
 const app = express();
 
 app.use(express.json());
+
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yml"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/temperature", temperatureRoutes);
 app.use("/api/humidity", humidityRoutes);
